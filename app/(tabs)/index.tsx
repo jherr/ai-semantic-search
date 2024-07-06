109118f9db064b23c7869fd14ffa629f6551f138
Jack Herrington <jherr@pobox.com>
2024-07-06 15:47:38 -0700

    Instrumentation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -77,38 +77,55 @@ async function buildIndex<
     synopsis: string;
   }
 >(docs: T[]) {
-  console.log("Building index");
+  const tensorFlowStartup = Date.now();
   await tf.ready();
+  console.log(`tensorFlowStartup: ${Date.now() - tensorFlowStartup}ms`);
+
+  const loadEncoder = Date.now();
   const use = require("@tensorflow-models/universal-sentence-encoder");
   const model = await use.load();
+  console.log(`loadEncoder: ${Date.now() - loadEncoder}ms`);
 
+  const createDocs = Date.now();
   const docsById = docs.reduce((acc, doc) => {
     acc[doc.id] = doc;
     return acc;
   }, {} as Record<number, T>);
+  console.log(`createDocs: ${Date.now() - createDocs}ms`);
 
+  const createEmbeddings = Date.now();
   const embeddingsTensor = await model.embed(
     docs.map((d) => `${d.title} ${d.synopsis}`)
   );
   const embeddings = embeddingsTensor.arraySync();
+  console.log(`createEmbeddings: ${Date.now() - createEmbeddings}ms`);
 
-  const hnsw = new HNSW(200, 16, 512, "cosine");
-
+  const createTreeData = Date.now();
   const data = [];
   for (const docIndex in docs) {
     const doc = docs[docIndex];
     const vector = embeddings[docIndex];
     data.push({ id: doc.id, vector });
   }
+  console.log(`createTreeData: ${Date.now() - createTreeData}ms`);
 
+  const createHSNW = Date.now();
+  const hnsw = new HNSW(200, 16, 512, "cosine");
   await hnsw.buildIndex(data);
+  console.log(`createHSNW: ${Date.now() - createHSNW}ms`);
 
   return {
     query: async (q: string, n: number = 3): Promise<T[]> => {
       if (q.trim() === "") return [];
+      const queryEmbed = Date.now();
       const queryTensor = await model.embed([q]);
       const vector = queryTensor.arraySync()[0];
+      console.log(`queryEmbed: ${Date.now() - queryEmbed}ms`);
+
+      const searchKNN = Date.now();
       const found = hnsw.searchKNN(vector, n);
+      console.log(`searchKNN: ${Date.now() - searchKNN}ms`);
+
       const sorted = found.sort((a, b) => (a.score > b.score ? -1 : 1));
       console.log(sorted);
       return sorted.map((f) => docsById[f.id]) as T[];
